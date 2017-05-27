024575ed524449693e8eb61f898d2fb67f8b8357
Lakshminarayana Nune <LN37868@TechMahindra.com>
2017-05-27 15:36:11 +0200

    commiting

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Lakshmi..CI!" );
+        System.exit( 0 ); //success
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# onegit
-My first git project repository
-this is my first line in readme file under git
